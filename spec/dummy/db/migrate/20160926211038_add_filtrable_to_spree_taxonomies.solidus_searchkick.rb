e7f47e9e8acc56f1837b7f0daa39b96728169de1
Jim Smith <jim@jimsmithdesign.com>
2016-09-28 10:35:11 -0400

    Removing spec/dummy from Git
    This should have never been committed in the first place, but was missed by the initial gitignore file.

--- spec/dummy/db/migrate/20160926211038_add_filtrable_to_spree_taxonomies.solidus_searchkick.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from solidus_searchkick (originally 20150819222417)
-class AddFiltrableToSpreeTaxonomies < ActiveRecord::Migration
-  def change
-    add_column :spree_taxonomies, :filterable, :boolean
-  end
-end
--- spec/dummy/db/migrate/20160926211039_add_filterable_to_spree_property.solidus_searchkick.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from solidus_searchkick (originally 20151009155442)
-class AddFilterableToSpreeProperty < ActiveRecord::Migration
-  def change
-    add_column :spree_properties, :filterable, :boolean
-  end
-end
